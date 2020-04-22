f5dd598db3abdbb50b2d6bbdb82a3f8600df540d
Chris Williams <chris.williams.ctr@gmail.com>
2020-04-22 08:40:59 -0400

    Updated classification scripts

--- RandomForestClassification.py
+++ RandomForestClassification.py
@@ -6,6 +6,6 @@ from classificationtemplate import X_train, y_train
 from sklearn.ensemble import RandomForestClassifier
 # assign the classifier to random forest
 # n_estimators is how many trees you want
-classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy')
 # fit the classifier to the training set
 classifier.fit(X_train, y_train)
